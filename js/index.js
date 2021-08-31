15ac42be7f2cec5d21c618750b74cb8fff734352
ConScorn <connorscorn@hotmail.com>
2021-08-31 03:40:36 -0500

    added time based styling

---
+++ js/index.js
@@ -0,0 +1,68 @@
+
+function currentDate() {
+    document.getElementById("currentDay").textContent = moment().format('dddd, MMMM Do YYYY');
+}
+
+function init() {
+    setHours();
+    currentDate();
+    
+}
+
+function setHours() {
+    let hour = new Date().getHours();
+
+    for (i = 8; i <= 16; i++) {
+
+        if ($(`#hour${i}`) == hour) {
+            $(`#hour${i}`).addClass("present");
+            console.log($(`#hour${i}`))
+        } else if ($(`#hour${i}`) < hour) {
+            $(`#hour${i}`).addClass("future");
+        } else {
+            $(`#hour${i}`).addClass("past");
+        }
+    }
+}
+
+document.getElementById('eight-btn').addEventListener('click', function() {
+    localStorage.setItem("8:00", document.getElementById('8-text').value);
+});
+
+document.getElementById('nine-btn').addEventListener('click', function() {
+    localStorage.setItem("9:00", document.getElementById('9-text').value);
+});
+
+document.getElementById('ten-btn').addEventListener('click', function() {
+    localStorage.setItem("10:00", document.getElementById('10-text').value);
+});
+
+document.getElementById('eleven-btn').addEventListener('click', function() {
+    localStorage.setItem("11:00", document.getElementById('11-text').value);
+});
+
+document.getElementById('twelve-btn').addEventListener('click', function() {
+    localStorage.setItem("12:00", document.getElementById('12-text').value);
+});
+
+document.getElementById('thirteen-btn').addEventListener('click', function() {
+    localStorage.setItem("1:00", document.getElementById('13-text').value);
+});
+
+document.getElementById('fourteen-btn').addEventListener('click', function() {
+    localStorage.setItem("2:00", document.getElementById('14-text').value);
+});
+
+document.getElementById('fifteen-btn').addEventListener('click', function() {
+    localStorage.setItem("3:00", document.getElementById('15-text').value);
+});
+
+document.getElementById('sixteen-btn').addEventListener('click', function() {
+    localStorage.setItem("4:00", document.getElementById('16-text').value);
+});
+
+function displaySavedTasks() {
+    $('#9-text').val(localStorage.getItem("9:00"));
+}
+
+init();
